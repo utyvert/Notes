81b6acfca231351ffff1d71682cd9326919d1b2c
utyvert <utkarshuppal@gmail.com>
2022-08-25 20:11:36 -0700

    Update 118 Pascal's Triangle.js

--- Leetcodes & Problems/118 Pascal's Triangle.js
+++ Leetcodes & Problems/118 Pascal's Triangle.js
@@ -1,12 +1,19 @@
 var generate = function(numRows) {
+    let pascal = [];
     for (let x = 0; x < numRows; x++){
-        let rowContainer = [];
-        
-
+        pascal[x] = [];
+        pascal[x][0] = 1;
+        for (let i = 1; i <= x; i++){
+            pascal[x][i] = pascal[x-1][i-1] + pascal[x-1][i]
+        }
 
     }
 
+    return pascal;
 };
 
 console.log(generate(5)) // should output '[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]'
 console.log(generate(1)) // should output [[1]]
+
+
+
