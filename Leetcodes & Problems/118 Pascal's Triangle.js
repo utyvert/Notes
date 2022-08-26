75f312022cffb2092548a96aaa3cf757a46f5b27
utyvert <utkarshuppal@gmail.com>
2022-08-25 18:11:23 -0700

    Create 118 Pascal's Triangle.js

---
+++ Leetcodes & Problems/118 Pascal's Triangle.js
@@ -0,0 +1,12 @@
+var generate = function(numRows) {
+    for (let x = 0; x < numRows; x++){
+        let rowContainer = [];
+        
+
+
+    }
+
+};
+
+console.log(generate(5)) // should output '[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]'
+console.log(generate(1)) // should output [[1]]
