488d2804858523c4f670067f5289cd6a3662d4e2
utyvert <utkarshuppal@gmail.com>
2022-08-27 02:01:41 -0700

    Update validParentheses20.js

--- Leetcodes-Problems/validParentheses20.js
+++ Leetcodes-Problems/validParentheses20.js
@@ -1,41 +1,53 @@
 const isValid = function(s) {
     const parArr = s.split('')
-    if (parArr.length % 2 === 1) return false;
+    if (parArr.length % 2 !== 0) return false;
     let container = [];
     let isVal = true;
 
     for (let i = 0; i < parArr.length; i ++) {
+
         if (parArr[i] === '{') {
             for (let x = i + 1; x < parArr.length; x++) {
-                if (parArr[x] === '}')
-                parArr.splice(i, 1);
-                parArr.splice(x, 1);
-                break;
-            }
-        } else if (parArr[i] === '(') {
-            for (let x = i + 1; x < parArr.length; x++) {
-                if (parArr[x] === ')')
-                parArr.splice(i, 1);
-                parArr.splice(x, 1);
-                break;
-            }
-        } else if (parArr[i] === '[') {
-            for (let x = i + 1; x < parArr.length; x++) {
-                if (parArr[x] === ']')
-                parArr.splice(i, 1);
-                parArr.splice(x, 1);
-                break;
+                if (parArr[x] === '}'){
+                    console.log(parArr)
+                    parArr.splice(x, 1);
+                    parArr.splice(i, 1);
+                    
+                    break;
+                }
             }
         }
-
-
-        return parArr;
+        
+        if (parArr[i] === '(') {
+            for (let x = i + 1; x < parArr.length; x++) {
+                if (parArr[x] === ')'){
+                    console.log(parArr)
+                    parArr.splice(x, 1);
+                    parArr.splice(i, 1);
+                    
+                    break;
+                }
+            }
+        }
+        
+        if (parArr[i] === '[') {
+            for (let x = i + 1; x < parArr.length; x++) {
+                if (parArr[x] === ']'){
+                    console.log(parArr)
+                    parArr.splice(x, 1);
+                    parArr.splice(i, 1);
+                    
+                    break;
+                }
+            }
+        }
     }
-
+    console.log(parArr)
+    return parArr.length === 0;
 };
 
 
 
-console.log(isValid('()')) // true
+// console.log(isValid('()')) // true
 console.log(isValid("()[]{}")) // true
-console.log(isValid("(]")) // false
+// console.log(isValid("(]")) // false
