d28ccfd756c40eaff0122fd50ac58a382a254110
utyvert <utkarshuppal@gmail.com>
2022-08-27 02:10:25 -0700

    refactored validParentheses, made it cleaner

--- Leetcodes-Problems/validParentheses20.js
+++ Leetcodes-Problems/validParentheses20.js
@@ -2,52 +2,23 @@ const isValid = function(s) {
     const parArr = s.split('')
     if (parArr.length % 2 !== 0) return false;
     let container = [];
-    let isVal = true;
-
-    for (let i = 0; i < parArr.length; i ++) {
-
-        if (parArr[i] === '{') {
-            for (let x = i + 1; x < parArr.length; x++) {
-                if (parArr[x] === '}'){
-                    console.log(parArr)
-                    parArr.splice(x, 1);
-                    parArr.splice(i, 1);
-                    
-                    break;
-                }
-            }
-        }
-        
-        if (parArr[i] === '(') {
-            for (let x = i + 1; x < parArr.length; x++) {
-                if (parArr[x] === ')'){
-                    console.log(parArr)
-                    parArr.splice(x, 1);
-                    parArr.splice(i, 1);
-                    
-                    break;
-                }
-            }
-        }
-        
-        if (parArr[i] === '[') {
-            for (let x = i + 1; x < parArr.length; x++) {
-                if (parArr[x] === ']'){
-                    console.log(parArr)
-                    parArr.splice(x, 1);
-                    parArr.splice(i, 1);
-                    
-                    break;
-                }
-            }
-        }
+    const map = { 
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+    for (i of parArr) {
+        if (!map[i]){
+            container.push(i);
+        } else if (container[container.length -1] === map[i]) container.pop();
+        else return false;
     }
-    console.log(parArr)
-    return parArr.length === 0;
+
+    return container.length === 0;
 };
 
 
 
-// console.log(isValid('()')) // true
+console.log(isValid('()')) // true
 console.log(isValid("()[]{}")) // true
-// console.log(isValid("(]")) // false
+console.log(isValid("(]")) // false
